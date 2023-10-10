c5d6a80f94fa0ed55a3e5ee253cd820db115a932
Andrés Ramírez <andres200493@gmail.com>
2023-10-09 21:38:11 -0600

    S2 -> E3

--- s2/src/Main.java
+++ s2/src/Main.java
@@ -1,3 +1,8 @@
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -45,11 +50,48 @@ public class Main {
 
 
 //        ---------------------R2------------------------
-        Thread t1 = new Thread(new Hilo1());
-        Thread t2 = new Thread(new Hilo2());
-        t2.setDaemon(true);
+//        Thread t1 = new Thread(new Hilo1());
+//        Thread t2 = new Thread(new Hilo2());
+//        t2.setDaemon(true);
+//
+//        t1.start();
+//        t2.start();
 
-        t1.start();
-        t2.start();
+
+//        ---------------------E3------------------------
+        ExecutorService pool = Executors.newCachedThreadPool(); // creamos un pool preconfigurado
+
+        String[] nombres = {"Uno", "Dos", "Tres"}; //los nombres para nuestros hilos
+        for (String nombre : nombres) {
+            pool.execute(new MiRunnablePool(nombre)); // creamos cada hilo y lo ejecutamos
+        }
+        System.out.println("Estado del pool antes de apagar: isShutdown()=" + pool.isShutdown() + ", isTerminated()=" + pool.isTerminated());
+        pool.shutdown(); // Apagamos el pool para que no pueda recibir nuevos hilos
+
+//pool.execute(new MiRunnablePool("Cuatro")); // si tratamos de hacer esto recibiremos una excepción
+        System.out.println("Estado del pool después de apagar: isShutdown()=" + pool.isShutdown() + ", isTerminated()=" + pool.isTerminated());
+
+        try {
+            long tiempoLimiteMs = 100;
+            System.out.println("Esperando que los hilos finalicen en " + tiempoLimiteMs + "ms...");
+
+            boolean terminaron = pool.awaitTermination(tiempoLimiteMs, TimeUnit.MILLISECONDS); //Esperamos a los hilos por 100ms, retorna true si acabaron antes o hasta los 100ms o false si fueron interrumpidos
+            System.out.println("¿Terminaron nuestros hilos? " +  terminaron);
+
+            if(!terminaron){
+                System.out.println("Llamando a shutdownNow()...");
+                List<Runnable> pendientes = pool.shutdownNow(); //Termina los hilos que se estén ejecutando y retorna una lista de hilos pendientes a ejecutarse
+                System.out.println(pendientes.size() + " hilos pendientes");
+                terminaron = pool.awaitTermination(tiempoLimiteMs, TimeUnit.MILLISECONDS); //esperando otros 100ms a que terminen nuestros hilos
+
+                if(!terminaron){
+                    System.out.println("Aún hay hilos pendientes");
+                }
+                System.out.println("Saliendo de main");
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
