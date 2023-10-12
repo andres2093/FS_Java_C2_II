55ec7ac141bfc94606fe512d8f277eca658d0ce5
Andrés Ramírez <andres200493@gmail.com>
2023-10-11 21:32:17 -0600

    S3 -> Extra

--- s3/src/test/java/org/bedu/jse2/demo/ejemplos/Ejemplo3Test.java
+++ s3/src/test/java/org/bedu/jse2/demo/ejemplos/Ejemplo3Test.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,4 +19,37 @@ class Ejemplo3Test {
         Integer expected = -27;
         assertThat(ejemplo3.manipular(conjunto, i -> i < 0 ? i : -i)).isEqualTo(expected);
     }
+
+    IntStream iterator = new Random().ints(100, 1, 20);
+    @Test
+    @DisplayName("Sumando numeros")
+    void sumandoNumeros(){
+        assertThat(iterator.sum()).isEqualTo(10);
+    }
+    @Test
+    @DisplayName("Revisar valores")
+    void revisarValores(){
+        System.out.println(Arrays.toString(iterator.toArray()));
+    }
+
+    List<String> lista = Arrays.asList("Antonio", "Maria", "Juan", "Pedro", "Rafael", "Salvador");
+
+    @Test
+    @DisplayName("Iterando con for")
+    void iterandoConFor(){
+        int i = 0;
+        for (String nombre : lista) {
+            if (nombre.contains("a")) {
+                i++;
+            }
+        }
+        assertThat(i).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("Iterando con stream")
+    void iterandoConStream(){
+        assertThat(lista.stream().filter(nombre -> nombre.contains("a")).count()).isEqualTo(4);
+    }
+
 }
