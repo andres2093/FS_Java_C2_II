b851c92ef44631717c51660145042e3b9acfee72
Andrés Ramírez <andres200493@gmail.com>
2023-10-11 19:36:55 -0600

    S3 - R1

--- s3/src/main/java/org/bedu/jse2/demo/ejemplos/Ejemplo1.java
+++ s3/src/main/java/org/bedu/jse2/demo/ejemplos/Ejemplo1.java
@@ -1,5 +1,7 @@
 package org.bedu.jse2.demo.ejemplos;
 
+import java.util.function.Function;
+
 public class Ejemplo1 {
 //    Con función anonima
 //    private final StringToInteger parser = new StringToInteger() {
@@ -12,9 +14,17 @@ public class Ejemplo1 {
 //    Con lambda
 //    private final StringToInteger parser = str -> Integer.parseInt(str);
 
+//    EJEMPLO 01
 //    Lambda con metodo de referencia
-    private final StringToInteger parser = Integer::parseInt;
-    Integer sumar(String a, String b){
-        return parser.convertir(a) + parser.convertir(b);
+//    private final StringToInteger parser = Integer::parseInt;
+//    Integer sumar(String a, String b){
+//        return parser.convertir(a) + parser.convertir(b);
+//    }
+
+//    RETO 01
+    private final Function<String, Integer> parser = Integer::parseInt;
+
+    Integer sumar(String a, String b) {
+        return parser.apply(a) + parser.apply(b);
     }
 }
