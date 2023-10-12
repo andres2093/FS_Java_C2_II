3f2f4ed5e8f39dac4eae22b85e9c8c66a6e8921d
Andrés Ramírez <andres200493@gmail.com>
2023-10-11 20:16:57 -0600

    S3 -> E2

---
+++ s3/src/main/java/org/bedu/jse2/demo/ejemplos/Ejemplo2.java
@@ -0,0 +1,16 @@
+package org.bedu.jse2.demo.ejemplos;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Function;
+
+public class Ejemplo2 {
+    List<Integer> transformar(Iterable<Integer> conjunto, Function<Integer, Integer> funcion){
+        List<Integer> lista = new ArrayList<>();
+        for (Integer valor : conjunto) {
+            lista.add(funcion.apply(valor));
+        }
+        return lista;
+    }
+}
