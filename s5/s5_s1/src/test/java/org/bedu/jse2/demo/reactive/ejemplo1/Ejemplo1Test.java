9f43d8716c99a6b242d82dce46ecef531615aa1a
Andrés Ramírez <andres200493@gmail.com>
2023-10-18 19:35:00 -0600

    S5 -> E1

--- s5/s5_s1/src/test/java/org/bedu/jse2/demo/reactive/ejemplo1/Ejemplo1Test.java
+++ s5/s5_s1/src/test/java/org/bedu/jse2/demo/reactive/ejemplo1/Ejemplo1Test.java
@@ -1,7 +1,23 @@
 package org.bedu.jse2.demo.reactive.ejemplo1;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Ejemplo1Test {
 
+    @Test
+    @DisplayName("Suma los elementos y regresa Single")
+    void sumarSingle() {
+        Ejemplo1.sumarSingle()
+                .subscribe(s -> assertThat(s).isEqualTo(21));
+    }
+
+    @Test
+    @DisplayName("Suma los elementos y regresa valor (bloqueante)")
+    void sumar() {
+        assertThat(Ejemplo1.sumar()).isEqualTo(21);
+    }
 }
