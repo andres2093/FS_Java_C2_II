4d6b9b8c1239854ec22c6903a1f05ea0e3d42914
Andrés Ramírez <andres200493@gmail.com>
2023-10-18 21:22:19 -0600

    S5 -> E3

--- s5/s5_s2/src/main/java/org/bedu/jse2/demo/reactive/ejemplo3/E3Service.java
+++ s5/s5_s2/src/main/java/org/bedu/jse2/demo/reactive/ejemplo3/E3Service.java
@@ -6,8 +6,13 @@ public class E3Service {
     private static final E3Repository REPOSITORY = new E3Repository();
 
     public Flux<String> getListaNombre(){
-        //Tu implementación va aquí
-        return null;
+        return REPOSITORY
+                .getPersonas()
+                .map(this::formatearNombre);
+    }
+
+    private String formatearNombre(PersonaEntity persona){
+        return String.format("%s %s, %s", persona.getPrimerApellido(), persona.getSegundoApellido(), persona.getNombre());
     }
 
 }
