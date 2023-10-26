0a757bd1ac350c27d04b05bbe6cb5d3a85b99e59
Andrés Ramírez <andres200493@gmail.com>
2023-10-25 20:56:26 -0600

    S7 -> R3

--- s7/s7.3/src/main/java/com/example/s73/controller/DemoController.java
+++ s7/s7.3/src/main/java/com/example/s73/controller/DemoController.java
@@ -3,11 +3,9 @@ package com.example.s73.controller;
 import com.example.s73.entity.Empleado;
 import com.example.s73.repository.EmpleadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/empleados")
@@ -19,5 +17,15 @@ public class DemoController {
     @GetMapping
     private Flux<Empleado> getAllEmpleados(){
         return empleadoRepository.findAllEmpleados();
+    }
+
+    @GetMapping("/{id}")
+    private Mono<Empleado> getEmpleadoById(@PathVariable String id) {
+        return empleadoRepository.findEmpleadoById(id);
+    }
+
+    @PutMapping
+    private Mono<Empleado> updateEmpleado(@RequestBody Empleado empleado) {
+        return empleadoRepository.updateEmpleado(empleado);
     }
 }
