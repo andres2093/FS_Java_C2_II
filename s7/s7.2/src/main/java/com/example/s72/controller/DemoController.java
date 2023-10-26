eb2318cd800aba15d60c9d89e9460591a02039e6
Andrés Ramírez <andres200493@gmail.com>
2023-10-25 19:47:42 -0600

    S7 -> R1

--- s7/s7.2/src/main/java/com/example/s72/controller/DemoController.java
+++ s7/s7.2/src/main/java/com/example/s72/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.example.s72.controller;
 
 import com.example.s72.entity.Auto;
+import com.example.s72.entity.Casa;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,5 +11,10 @@ public class DemoController {
     @PostMapping("/auto")
     public void recibe(@RequestBody Auto auto){
         System.out.println(auto.toString());
+    }
+
+    @PostMapping("/casa")
+    public void recibeCasa(@RequestBody Casa casa){
+        System.out.println(casa.toString());
     }
 }
---
+++ s7/s7.2/src/main/java/com/example/s72/entity/Casa.java
@@ -0,0 +1,11 @@
+package com.example.s72.entity;
+
+import lombok.Data;
+
+@Data
+public class Casa {
+
+    private int id;
+    private String color;
+    private int pisos;
+}
