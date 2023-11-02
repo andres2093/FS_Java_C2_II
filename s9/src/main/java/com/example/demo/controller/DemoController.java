06742ddcf999b055bb853342ba2ee6d0d38e29e7
Andrés Ramírez <andres200493@gmail.com>
2023-11-01 20:03:09 -0600

    S9 -> no crear empleado si ya existe uno con el mismo id

--- s9/src/main/java/com/example/demo/controller/DemoController.java
+++ s9/src/main/java/com/example/demo/controller/DemoController.java
@@ -31,8 +31,11 @@ public class DemoController {
 
     @PostMapping
     private Mono<Empleado> crear(@RequestBody Empleado empleado) {
-        System.out.println(empleado.getId());
-        System.out.println(empleado.getNombre());
-        return empleadoRepository.save(empleado);
+        return empleadoRepository.findById(empleado.getId())
+                .flatMap(empleadoRegistrado -> Mono.error(
+                        new Exception(String.format("Empleado con el id %s ya existe!!!", empleadoRegistrado.getId())))
+                )
+                .cast(Empleado.class)
+                .switchIfEmpty(empleadoRepository.save(empleado));
     }
 }
